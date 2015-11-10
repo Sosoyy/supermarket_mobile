cf4e438b356a8aadfe5d942abee755f38ca24798
Soso <610204448@qq.com>
2015-11-10 09:48:21 +0800

    超市触屏版 登录注册相关界面

---
+++ js/style.js
@@ -0,0 +1,8 @@
+/**
+ * User: huyy
+ * Date: 2015/11/9 0009
+ * Time: обнГ 5:17
+ */
+var swiper = new Swiper('.u-scrollcontent', {
+    loop: true, autoplay: 2000, pagination: '.swiper-pagination',
+});
